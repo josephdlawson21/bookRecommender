5a17760d5293c385e150314f4726890a8211df09
johneckert <johnteckert@gmail.com>
2018-02-12 11:48:14 -0500

    building controllers and routes

--- bookRecommender_Backend/config/routes.rb
+++ bookRecommender_Backend/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only:[:show, :create]
+  resources :books, only:[:show, :create]
+
+  GET '/users/:id/books', to: 'user#getbooks'
+  POST '/user/:id/books/:id', to: 'user#postbooks'
+
+
 end
